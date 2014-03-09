d0203354707d4ef206ba1790e07379dccca8ea42
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-03-09 08:48:41 +0000

    solution to q5
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@325 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

---
+++ workshops/week1/q5/q5.py
@@ -0,0 +1,53 @@
+
+import math
+import coll
+from collections import Counter
+
+def get_doc_freqs(coll_data):
+	dfs = Counter()
+	for doc_id in coll_data.get_docs():
+		doc = coll_data.get_doc(doc_id)
+		dfs.update(dict(zip(doc.terms.keys(), [1]*len(doc.terms))))
+	return dfs
+
+def get_inverse_doc_freqs(coll_data):
+	res = {}
+	dfs = get_doc_freqs(coll_data)
+	num_docs = coll_data.get_num_docs()
+	for term, df in dfs.items():
+		res[term] = math.log(num_docs) - math.log(df)
+	return res
+
+def get_doc_tf_idf(doc_id, idfs, coll_data):
+	res = {}
+	doc = coll_data.get_doc(doc_id)
+	#l2 length
+	term_vector_length = math.sqrt(sum(term_count**2 for term_count in doc.terms.values()))
+	for term, term_count in doc.terms.items():
+		term_weight = math.log(1 + (term_count/term_vector_length))
+		res[term] = term_weight*idfs[term]
+	return res
+
+def cosine_similarity(weight_dict_1, weight_dict_2):
+	'''
+	assumes that both weight_dict_1 and weight_dict_2 are normalized
+	'''
+	score = 0
+	for term in set(weight_dict_1.keys()).intersection(weight_dict_2.keys()):
+		score += weight_dict_1[term]*weight_dict_2[term]
+	return score
+
+def doc_similarity(doc_id_1, doc_id_2, distance_func, idfs, coll_data):
+	return distance_func(
+		get_doc_tf_idf(doc_id_1, idfs, coll_data),
+		get_doc_tf_idf(doc_id_2, idfs, coll_data)
+	)
+
+def main():
+	coll_data = coll.parse_lyrl_coll('../../../../data/lyrl_tokens_30k.dat')
+	idfs = get_inverse_doc_freqs(coll_data)
+	print(doc_similarity('26152', '26159', cosine_similarity, idfs, coll_data))
+	print(doc_similarity('26152', '26413', cosine_similarity, idfs, coll_data))
+
+if __name__ == '__main__':
+	main()
