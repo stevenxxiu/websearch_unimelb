fd0b465da2c01c70ceff0f5bcd1e159134fc8950
Steven Xu <>
2014-03-09 18:28:21 +1100

    solution to q2

--- workshops/week1/q1/q1.py
+++ workshops/week1/q1/q1.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env/python
 
 import coll
 import numpy as np
---
+++ workshops/week1/q2/q2.py
@@ -0,0 +1,20 @@
+
+import coll
+from collections import Counter
+
+def get_doc_freqs(coll_data):
+	dfs = Counter()
+	for doc_id in coll_data.get_docs():
+		doc = coll_data.get_doc(doc_id)
+		dfs.update(dict(zip(doc.terms.keys(), [1]*len(doc.terms))))
+	return dfs
+
+def main():
+	coll_data = coll.parse_lyrl_coll('../../../../data/lyrl_tokens_30k.dat')
+	dfs_sorted = sorted(get_doc_freqs(coll_data).items(), key=lambda t: (-t[1], t[0]))
+	print('{:<50}{:}'.format('TERM', 'DOC_FREQ'))
+	for term, freqs in dfs_sorted:
+		print('{:<50}{:}'.format(term, freqs))
+
+if __name__ == '__main__':
+	main()
