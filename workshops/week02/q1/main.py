6d2f65a0a3cac3dbb554850252dfbfbe40a2bfb8
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-03-13 13:48:08 +0000

    solution to week02 q1
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@510 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

---
+++ workshops/week02/q1/main.py
@@ -0,0 +1,28 @@
+
+import sys
+sys.path.append('../lib')
+import pymongo
+
+def cosine_similarity(weight_dict_1, weight_dict_2):
+	'''
+	assumes that both weight_dict_1 and weight_dict_2 are normalized
+	'''
+	score = 0
+	for term in set(weight_dict_1.keys()).intersection(weight_dict_2.keys()):
+		score += weight_dict_1[term]*weight_dict_2[term]
+	return score
+
+def doc_similarity(doc_id_1, doc_id_2, distance_func, db_collection):
+	return distance_func(
+		db_collection.find_one({'doc_id': doc_id_1})['weights'],
+		db_collection.find_one({'doc_id': doc_id_2})['weights']
+	)
+
+def main():
+	client = pymongo.MongoClient()
+	collection = client['websearch_workshops']['week02']
+	print(doc_similarity('26152', '26159', cosine_similarity, collection))
+	print(doc_similarity('26152', '26413', cosine_similarity, collection))
+
+if __name__ == '__main__':
+	main()
