45a61945ad0e0c089f0d003d3e9ec5b3cc6948f0
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-03-18 11:34:28 +0000

    updated database structure for q1 to be similar to that of q2
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@761 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- workshops/week02/q1/main.py
+++ workshops/week02/q1/main.py
@@ -13,18 +13,18 @@ def cosine_similarity(weight_dict_1, weight_dict_2):
 		score += weight_dict_1[term]*weight_dict_2[term]
 	return score
 
-def doc_similarity(doc_id_1, doc_id_2, distance_func, db_collection):
+def doc_similarity(doc_id_1, doc_id_2, docs_db, distance_func):
 	return distance_func(
-		db_collection.find_one({'doc_id': doc_id_1})['weights'],
-		db_collection.find_one({'doc_id': doc_id_2})['weights']
+		docs_db.find_one({'doc_id': doc_id_1})['weights'],
+		docs_db.find_one({'doc_id': doc_id_2})['weights']
 	)
 
 def main():
 	start=time.clock()
 	client = pymongo.MongoClient()
-	collection = client['websearch_workshops']['week02_tfidf_docs']
-	print(doc_similarity('26152', '26159', cosine_similarity, collection))
-	print(doc_similarity('26152', '26413', cosine_similarity, collection))
+	collection = client['websearch_workshops']['week02']['tfidf']
+	print(doc_similarity('26152', '26159', collection, cosine_similarity))
+	print(doc_similarity('26152', '26413', collection, cosine_similarity))
 	print('Took {:.6f} seconds'.format(time.clock()-start))
 
 if __name__ == '__main__':
