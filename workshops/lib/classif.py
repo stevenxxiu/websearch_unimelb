b1fedd89a98eb81a7ab531916cc4fdc54bfec9af
Steven Xu <>
2014-04-07 14:15:22 +1000

    solution to q6

--- workshops/lib/classif.py
+++ workshops/lib/classif.py
@@ -39,22 +39,21 @@ class KNN:
         self.docs_db = docs_db
 
     def classify(self, weights, k):
-        res = {}
         closest = []
         # find the nearest k training documents
         for doc_id, doc_classes in self.train_docs:
             dist = self.distance_func(weights, self.docs_db.find_one({'doc_id': doc_id})['weights'])
-            heapq.heappush(closest, (dist, doc_id))
+            heapq.heappush(closest, (-dist, doc_id))
             if len(closest)>k:
                 heapq.heappop(closest)
         res = {}
         train_docs_dict = dict(self.train_docs)
-        for dist, doc_id in res:
+        for dist, doc_id in closest:
             for doc_class in train_docs_dict[doc_id]:
                 res.setdefault(doc_class, 0)
                 res[doc_class] += 1
         num_classes = sum(res.values())
         for doc_class in res:
             res[doc_class]/=num_classes
-        return sorted(res.items(), key=lambda doc_score: (doc_score[1], doc_score[0]))
+        return sorted(res.items(), key=lambda doc_score: (doc_score[1], doc_score[0]), reverse=True)
 
