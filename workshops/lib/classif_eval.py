e8be9f70d3a98a06ffbecd28df6c10b5e3cea063
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-07 08:37:03 +0000

    factored out ConfusionMatrix
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1332 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

---
+++ workshops/lib/classif_eval.py
@@ -0,0 +1,39 @@
+
+class ConfusionMatrix:
+    def __init__(self, tp, fp, tn, fn):
+        self.tp = tp
+        self.fp = fp
+        self.tn = tn
+        self.fn = fn
+
+    @classmethod
+    def generate(cls, classif_docs, test_docs, pos_class, neg_class):
+        tp = fp = tn = fn = 0
+        for classif_doc, test_doc in zip(classif_docs, test_docs):
+            classif_id, classif_class = classif_doc
+            test_id, test_class = test_doc
+            if classif_id != test_id:
+                raise ValueError('id mismatch')
+            if classif_class == pos_class and test_class == pos_class:
+                tp += 1
+            elif classif_class == pos_class and test_class == neg_class:
+                fp += 1
+            elif classif_class == neg_class and test_class == pos_class:
+                fn += 1
+            elif classif_class == neg_class and test_class == neg_class:
+                tn += 1
+        return cls(tp, fp, tn, fn)
+
+    def accuracy(self):
+        tp, fp, tn, fn = self.tp, self.fp, self.tn, self.fn
+        try:
+            return (tp+tn)/(tp+fp+tn+fn)
+        except ZeroDivisionError:
+            return 0
+
+    def f1(self, beta=1.0):
+        tp, fp, tn, fn = self.tp, self.fp, self.tn, self.fn
+        try:
+            return ((1 + beta**2)*tp)/((1 + beta**2)*tp + beta**2*fn + fp)
+        except ZeroDivisionError:
+            return 0
--- workshops/week05/q4/main.py
+++ workshops/week05/q4/main.py
@@ -3,36 +3,36 @@ import time
 import pymongo
 from workshops.lib.weights import cosine_similarity
 from workshops.lib.classif import Rocchio
+from workshops.lib.classif_eval import ConfusionMatrix
 from workshops.lib.classif_data import parse_lyrl_topics
 
 def to_binary_classes(docs, doc_class):
     return list((doc_id, [doc_class]) if doc_class in doc_classes else (doc_id, ['not-{}'.format(doc_class)]) for doc_id, doc_classes in docs)
 
+def classif_binary(test_docs, classifier, tfidf_db):
+    for doc_id, doc_classes in test_docs:
+        classif_class, classif_dist = classifier.classify(tfidf_db.find_one({'doc_id': doc_id})['weights'])[0]
+        yield doc_id, classif_class
+
 def main():
     start = time.clock()
+    # get data
     client = pymongo.MongoClient()
     tfidf_db = client['websearch_workshops']['lyrl']['tfidf']
     classif_data = parse_lyrl_topics('../../../../data/lyrl30k_tpcs.txt')
     train_docs, test_docs = classif_data.get_train_test(15000, 15000)
     train_docs = to_binary_classes(train_docs, 'GCAT')
     test_docs = to_binary_classes(test_docs, 'GCAT')
+    test_docs_bin = ((doc_id, doc_classes[0]) for doc_id, doc_classes in test_docs)
+    # classify
     classifier = Rocchio(lambda w1, w2: 1-cosine_similarity(w1, w2))
     classifier.train(train_docs, tfidf_db)
-    tp = fp = tn = fn = 0
-    for doc_id, doc_classes in test_docs:
-        doc_class = doc_classes[0]
-        classif_class, classif_dist = classifier.classify(tfidf_db.find_one({'doc_id': doc_id})['weights'])[0]
-        if classif_class == 'GCAT' and doc_class == 'GCAT':
-            tp += 1
-        elif classif_class == 'GCAT' and doc_class != 'GCAT':
-            fp += 1
-        elif classif_class != 'GCAT' and doc_class == 'GCAT':
-            fn += 1
-        elif classif_class != 'GCAT' and doc_class != 'GCAT':
-            tn += 1
-    print('TP: {}, FP: {}, TN: {}, FN: {}'.format(tp, fp, tn, fn))
-    print('Accuracy: {}'.format((tp+tn)/(tp+fp+tn+fn)))
-    print('F1 Score: {}'.format(2*tp/(2*tp+fp+fn)))
+    classif_docs_bin = classif_binary(test_docs, classifier, tfidf_db)
+    # results
+    cf = ConfusionMatrix.generate(classif_docs_bin, test_docs_bin, 'GCAT', 'not-GCAT')
+    print('TP: {}, FP: {}, TN: {}, FN: {}'.format(cf.tp, cf.fp, cf.tn, cf.fn))
+    print('Accuracy: {}'.format(cf.accuracy()))
+    print('F1 Score: {}'.format(cf.f1()))
     print('Took {:.6f} seconds'.format(time.clock()-start))
 
 if __name__ == '__main__':
