6dc0f71ab1214977b386204b17f65b1d3e380666
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-07 08:33:21 +0000

    added plotting capability
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1295 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- workshops/week03/q3/main.py
+++ workshops/week03/q3/main.py
@@ -1,15 +1,28 @@
 
+import sys
 import time
 import pymongo
+import matplotlib.pyplot as plt
 from workshops.lib.cluster import cluster_aggloromotive_mst, cluster_aggloromotive_naive
 from workshops.lib.similarity import cosine_similarity
 
 def main():
-    start=time.clock()
+    sys.setrecursionlimit(4000)
     client = pymongo.MongoClient()
     tfidf_db = client['websearch_workshops']['week02']['tfidf']
-    cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity)
-    print('Took {:.6f} seconds'.format(time.clock()-start))
+    ns = [100, 200, 300, 400, 500, 600, 700, 800, 1600, 3200]
+    deltas = []
+    for n in ns:
+        docs = list(tfidf_db.find()[:n])
+        start = time.clock()
+        cluster_aggloromotive_mst(docs, cosine_similarity)
+        delta = time.clock()-start
+        deltas.append(delta)
+        print('{} docs took {:.6f} seconds'.format(n, delta))
+    plt.plot(ns, deltas, marker='o', linestyle='None')
+    plt.xlabel('n')
+    plt.ylabel('Time taken')
+    plt.show()
 
 if __name__ == '__main__':
     main()
