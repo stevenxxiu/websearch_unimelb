bb6235a1c37b1715500bec20964bf9ae56d2df08
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-07 08:35:41 +0000

    factored out get_dfs(), get_idfs(), get_doc_tf_idf_raw(), get_doc_tf_idf()
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1316 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- workshops/week01/q3/main.py
+++ workshops/week01/q3/main.py
@@ -1,35 +1,12 @@
 
-import math
-from collections import Counter
 from workshops.lib import coll
-
-def get_doc_freqs(coll_data):
-	dfs = Counter()
-	for doc_id in coll_data.get_docs():
-		doc = coll_data.get_doc(doc_id)
-		dfs.update(dict(zip(doc.terms.keys(), [1]*len(doc.terms))))
-	return dfs
-
-def get_inverse_doc_freqs(coll_data):
-	res = {}
-	dfs = get_doc_freqs(coll_data)
-	num_docs = coll_data.get_num_docs()
-	for term, df in dfs.items():
-		res[term] = math.log(num_docs) - math.log(df)
-	return res
-
-def get_doc_tf_idf(doc_id, idfs, coll_data):
-	res = {}
-	for term, term_count in coll_data.get_doc(doc_id).terms.items():
-		term_weight = math.log(1 + term_count)
-		res[term] = term_weight*idfs[term]
-	return res
+from workshops.lib.features import get_idfs, get_doc_tf_idf_raw
 
 def main():
 	coll_data = coll.parse_lyrl_coll('../../../../data/lyrl_tokens_30k.dat')
-	idfs = get_inverse_doc_freqs(coll_data)
+	idfs = get_idfs(coll_data)
 	for doc_id in coll_data.get_docs():
-		print(doc_id, get_doc_tf_idf(doc_id, idfs, coll_data))
+		print(doc_id, get_doc_tf_idf_raw(doc_id, idfs, coll_data))
 
 if __name__ == '__main__':
 	main()
