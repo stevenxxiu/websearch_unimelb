0a8f93cb3bbb655e97528621b44359602970cea3
Steven Xu <>
2014-04-07 10:35:17 +1000

    use labelled docs instead of dicts in the classifiers, if a classifier needs a dict it can construct it itself, this isn't necessary for many classifiers, and is closer to the scikits learn matrix format

--- workshops/lib/classif.py
+++ workshops/lib/classif.py
@@ -6,9 +6,10 @@ class Rocchio:
         self.class_mean_weights = {}
         self.distance_func = distance_func
 
-    def train(self, train_dict, docs_db):
+    def train(self, train_docs, docs_db):
         mean_weights = {}
-        for doc_class, doc_ids in train_dict.items():
+        train_docs_dict = dict(train_docs)
+        for doc_class, doc_ids in train_docs_dict.items():
             mean_weights[doc_class] = sum(WeightDict(docs_db.find_one({'doc_id': doc_id})['weights']) for doc_id in doc_ids)/len(doc_ids)
         self.class_mean_weights = mean_weights
 
