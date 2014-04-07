af0c706429db4684ef78f317a9ab02972566272b
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-07 08:03:52 +0000

    added assertion test
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1289 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- workshops/week03/q3/main.py
+++ workshops/week03/q3/main.py
@@ -1,14 +1,16 @@
 
 import time
 import pymongo
-from cluster import cluster_aggloromotive_mst, cluster_aggloromotive_naive
-from similarity import cosine_similarity
+from workshops.lib.cluster import cluster_aggloromotive_mst, cluster_aggloromotive_naive
+from workshops.lib.similarity import cosine_similarity
 
 def main():
     start=time.clock()
     client = pymongo.MongoClient()
     tfidf_db = client['websearch_workshops']['week02']['tfidf']
-    cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity)
+    assert cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity) == cluster_aggloromotive_naive(list(tfidf_db.find()[:100]), cosine_similarity)
+
+    # cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity)
 
     print('Took {:.6f} seconds'.format(time.clock()-start))
 
