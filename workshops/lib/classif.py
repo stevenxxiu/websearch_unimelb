aa4bc43d7a1a194cf1a10099e1adcfaf69b784f7
Steven Xu <>
2014-04-07 12:29:25 +1000

    solution to q3

--- workshops/lib/classif.py
+++ workshops/lib/classif.py
@@ -7,14 +7,21 @@ class Rocchio:
         self.distance_func = distance_func
 
     def train(self, train_docs, docs_db):
+        class_nums = {}
+        total_weights = {}
+        for doc_id, doc_classes in train_docs:
+            for doc_class in doc_classes:
+                total_weights.setdefault(doc_class, WeightDict())
+                total_weights[doc_class] += WeightDict(docs_db.find_one({'doc_id': doc_id})['weights'])
+                class_nums.setdefault(doc_class, 0)
+                class_nums[doc_class] += 1
         mean_weights = {}
-        train_docs_dict = dict(train_docs)
-        for doc_class, doc_ids in train_docs_dict.items():
-            mean_weights[doc_class] = sum(WeightDict(docs_db.find_one({'doc_id': doc_id})['weights']) for doc_id in doc_ids)/len(doc_ids)
+        for doc_class in class_nums.keys():
+            mean_weights[doc_class] = total_weights[doc_class]/class_nums[doc_class]
         self.class_mean_weights = mean_weights
 
     def classify(self, weights):
         res = {}
         for doc_class, mean_weights in self.class_mean_weights.items():
             res[doc_class] = self.distance_func(weights, mean_weights)
-        return sorted(res.items())
+        return sorted(res.items(), key=lambda doc_score: (doc_score[1], doc_score[0]))
---
+++ workshops/week05/q3/answer.tm
@@ -0,0 +1,30 @@
+<TeXmacs|1.99.1>
+
+<style|generic>
+
+<\body>
+  <section|Results>
+
+  Rocchio classified incorrectly 52.53% labels.
+
+  \;
+</body>
+
+<initial|<\collection>
+</collection>>
+
+<\references>
+  <\collection>
+    <associate|auto-1|<tuple|1|?>>
+  </collection>
+</references>
+
+<\auxiliary>
+  <\collection>
+    <\associate|toc>
+      <vspace*|1fn><with|font-series|<quote|bold>|math-font-series|<quote|bold>|1<space|2spc>P@k
+      User Model> <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
+      <no-break><pageref|auto-1><vspace|0.5fn>
+    </associate>
+  </collection>
+</auxiliary>
