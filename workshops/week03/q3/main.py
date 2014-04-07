79b48081c8601308fb68889ef351eceeee12a4d3
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-07 08:33:11 +0000

    moved assert into unit tests
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1293 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- workshops/week03/q3/main.py
+++ workshops/week03/q3/main.py
@@ -8,10 +8,7 @@ def main():
     start=time.clock()
     client = pymongo.MongoClient()
     tfidf_db = client['websearch_workshops']['week02']['tfidf']
-    assert cluster_aggloromotive_mst(list(tfidf_db.find()[:10]), cosine_similarity) == cluster_aggloromotive_naive(list(tfidf_db.find()[:10]), cosine_similarity)
-
-    # cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity)
-
+    cluster_aggloromotive_mst(list(tfidf_db.find()[:100]), cosine_similarity)
     print('Took {:.6f} seconds'.format(time.clock()-start))
 
 if __name__ == '__main__':
