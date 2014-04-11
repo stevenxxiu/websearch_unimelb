da46f937aa477c2601cbaaeeff5f5455ebb74499
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-04-11 16:13:08 +0000

    solution to q3
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@1390 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- proj1/lib/weights.py
+++ proj1/lib/weights.py
@@ -12,13 +12,35 @@ class WeightDict(Counter):
     def __truediv__(self, other):
         return self*(1/other)
 
-def normalize_weights(weights):
+def l2_dist(weights):
+    return math.sqrt(sum(v**2 for v in weights.values()))
+
+def l2_norm(weights):
     res = {}
-    # l2 norm
-    norm = math.sqrt(sum(v**2 for v in weights.values()))
+    norm = l2_dist(weights)
     for key, value in weights.items():
         res[key] = value/norm
     return res
+
+class PivotedLengthNorm:
+    def __init__(self, distance_func, docs_weights):
+        self.distance_func = distance_func
+        # find doc_length_average
+        doc_length_total = 0
+        i=None
+        for i, weights in enumerate(docs_weights):
+            doc_length_total += distance_func(weights)
+        if i is not None:
+            # don't use len() in case docs_weights is an iterator
+            doc_length_total/=i+1
+        self.doc_length_average = doc_length_total
+
+    def norm(self, weights, slope):
+        res = {}
+        alpha = ((1-slope)*self.doc_length_average + slope*self.distance_func(weights))
+        for key, value in weights.items():
+            res[key] = value/alpha
+        return res
 
 def cosine_similarity(weight_dict_1, weight_dict_2):
     '''
