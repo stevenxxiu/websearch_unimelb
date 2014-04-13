5e8526b620b8518cd65054e969b37d98d4a53f7b
Steven Xu <xxu@student.unimelb.edu.au>
2014-04-13 16:21:28 +1000

    correct solution to q3

--- proj1/lib/weights.py
+++ proj1/lib/weights.py
@@ -11,23 +11,9 @@ def l2_norm_sparse(X):
     norms = np.sqrt(norms.sum(axis=1))
     return diags(1/norms.A1, 0) * X
 
-
-class PivotedLengthNorm:
-    def __init__(self, distance_func, docs_weights):
-        self.distance_func = distance_func
-        # find doc_length_average
-        doc_length_total = 0
-        i=None
-        for i, weights in enumerate(docs_weights):
-            doc_length_total += distance_func(weights)
-        if i is not None:
-            # don't use len() in case docs_weights is an iterator
-            doc_length_total/=i+1
-        self.doc_length_average = doc_length_total
-
-    def norm(self, weights, slope):
-        res = {}
-        alpha = ((1-slope)*self.doc_length_average + slope*self.distance_func(weights))
-        for key, value in weights.items():
-            res[key] = value/alpha
-        return res
+def pivoted_length_norm(X, s, dataset):
+    F = X.copy()
+    F.data **= 2
+    doc_lengths = np.sqrt(F.sum(axis=1))
+    norms = (1-s)*doc_lengths.sum(axis=0)/X.shape[0] + s*doc_lengths
+    return diags(1/norms.A1, 0) * X
