a0e8c958d73e956c12f848a379f53b552e68fce6
Steven Xu <xxu@student.unimelb.edu.au>
2014-04-18 22:48:35 +1000

    tidied up code for build_gensim_index.py

--- workshops/bin/build_gensim_index.py
+++ workshops/bin/build_gensim_index.py
@@ -1,10 +1,8 @@
 
+import sys
 from workshops.lib.gensim_index import make_index
 
-if __name__ == '__main__':
-
-    import sys
-
+def main():
     if len(sys.argv) != 3:
         sys.stderr.write("USAGE: %s <lyrl-fname> <index-tag>\n" % (sys.argv[0]))
         sys.exit(1)
@@ -12,3 +10,7 @@ if __name__ == '__main__':
     lyrl_fname = sys.argv[1]
     index_tag = sys.argv[2]
     make_index(lyrl_fname, index_tag)
+
+
+if __name__ == '__main__':
+    main()
