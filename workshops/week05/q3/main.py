9d5be98a35023a730fcb62209b6bdad2b1e44edf
Steven Xu <xxu@student.unimelb.edu.au>
2014-04-20 22:37:44 +1000

    use matrices for week 5 q3

--- workshops/week05/q3/main.py
+++ workshops/week05/q3/main.py
@@ -1,25 +1,43 @@
 
 import time
-import pymongo
-from workshops.lib.weights import cosine_similarity
-from workshops.lib.classif import Rocchio
-from workshops.lib.classif_data import parse_lyrl_topics
+import pickle
+import numpy as np
+from scipy.sparse import vstack
+from workshops.lib.classif import get_train_test, Rocchio
+from workshops.lib.weights import l2_norm_sparse
+from workshops.lib.features import get_tf_idf
 
 def main():
     start = time.clock()
-    client = pymongo.MongoClient()
-    tfidf_db = client['websearch_workshops']['lyrl']['tfidf']
-    classif_data = parse_lyrl_topics('../../../../data/lyrl30k_tpcs.txt')
-    train_docs, test_docs = classif_data.get_train_test(15000, 15000)
-    classifier = Rocchio(lambda w1, w2: 1-cosine_similarity(w1, w2))
-    classifier.train(train_docs, tfidf_db)
-    num_incorrect = 0
-    for doc_id, doc_classes in test_docs:
-        classif_class, classif_dist = classifier.classify(tfidf_db.find_one({'doc_id': doc_id})['weights'])[0]
-        if classif_class not in doc_classes:
-            num_incorrect += 1
-    print('Classified incorrectly {:.2f}% labels'.format(num_incorrect/len(test_docs)*100))
-    print('Took {:.6f} seconds'.format(time.clock()-start))
+    with open('../../../../data/pickle/lyrl.db', 'rb') as docs_sr, open('../../../../data/pickle/lyrl_classif.db', 'rb') as classif_sr:
+        # noinspection PyArgumentList
+        docs_data = pickle.load(docs_sr)
+        # noinspection PyArgumentList
+        classif_data = pickle.load(classif_sr)
+        tf_idfs = l2_norm_sparse(get_tf_idf(docs_data.freq_matrix))
+        train_indexes, test_indexes = get_train_test(len(docs_data.docs), 15000, 15000)
+        train_X, train_Y = tf_idfs[train_indexes], classif_data.labels_matrix[train_indexes]
+        test_X, test_Y = tf_idfs[test_indexes], classif_data.labels_matrix[test_indexes]
+        # flatten train_Y by counting the same documents multiple times
+        train_doc_rows = []
+        train_label_rows = []
+        for train_doc_row, train_label_row in zip(train_X, train_Y):
+            for label in train_label_row.nonzero()[1]:
+                train_doc_rows.append(train_doc_row)
+                train_label_rows.append(label)
+        train_X = vstack(train_doc_rows, format='csr')
+        train_y = np.array(train_label_rows)
+        # classify
+        classifier = Rocchio(lambda X, Y: 1-X*Y.T)
+        classifier.fit(train_X, train_y)
+        predict_y = classifier.predict(test_X)
+        # get accuracy
+        num_incorrect = 0
+        for test_label_row, predict_label in zip(test_Y, predict_y):
+            if not test_label_row[:,predict_label].data:
+                num_incorrect += 1
+        print('Classified incorrectly {:.2f}% labels'.format(num_incorrect/test_X.shape[0]*100))
+        print('Took {:.6f} seconds'.format(time.clock()-start))
 
 if __name__ == '__main__':
     main()
--- workshops/week06/q2/main.py
+++ workshops/week06/q2/main.py
@@ -12,10 +12,10 @@ def main():
     classif_data = parse_lyrl_topics('../../../../data/lyrl30k_tpcs.txt')
     train_docs, test_docs = classif_data.get_train_test(15000, 15000)
     classifier = Rocchio(lambda w1, w2: 1-cosine_similarity(w1, w2))
-    classifier.train(train_docs, tfidf_db)
+    classifier.fit(train_docs, tfidf_db)
     num_incorrect = 0
     for doc_id, doc_classes in test_docs:
-        classif_class, classif_dist = classifier.classify(tfidf_db.find_one({'doc_id': doc_id})['weights'])[0]
+        classif_class, classif_dist = classifier.predict(tfidf_db.find_one({'doc_id': doc_id})['weights'])[0]
         if classif_class not in doc_classes:
             num_incorrect += 1
     print('Classified incorrectly {:.2f}% labels'.format(num_incorrect/len(test_docs)*100))
