77c5d0a18645fc64529354dd9fbd691ba246fc4d
Steven Xu <xxu@student.unimelb.edu.au>
2014-06-16 22:10:10 +1000

    added power iteration

---
+++ proj2/lib/eig.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+
+def power_iter(X, max_iter=100, tol=1.0e-8):
+    '''
+    gets the first principal axis using power iteration
+    doesn't converge if the maximum eigenvalue has multiplicity > 1, but still can be used as an approximation tool
+    '''
+    b = np.random.random_sample((X.shape[0], 1))
+    for i in range(max_iter):
+        b_prev = b
+        b = X * b
+        # normalize b
+        b *= 1/np.sqrt(np.sum(np.power(b, 2)))
+        # l1 norm
+        err = np.sum(b - b_prev)
+        if abs(err) < tol:
+            break
+    else:
+        raise ValueError('power iteration failed to converge in {} iterations'.format(max_iter))
+    return b
---
+++ proj2/test/test_eig.py
@@ -0,0 +1,8 @@
+
+import unittest
+import numpy as np
+from proj2.lib import eig
+
+class TestEigen(unittest.TestCase):
+    def test_power_iter(self):
+        self.assertTrue(np.allclose(eig.power_iter(np.matrix([[1, 2], [3, 4]])), [[ 0.41597356], [ 0.90937671]]))
