6b91197549147996f241999960bc0dc552d0c038
Steven Xu <xxu@student.unimelb.edu.au>
2014-06-18 01:15:32 +1000

    fixed unit tests

--- proj2/test/test_eig.py
+++ proj2/test/test_eig.py
@@ -11,4 +11,4 @@ class TestEigen(unittest.TestCase):
     # noinspection PyCallingNonCallable
     def test_first_sparse_pca(self):
         X = np.matrix([[ 1,  1], [2,  4], [3, 9], [ 4,  16], [ 5,  25], [ 6,  36]])
-        self.assertTrue(np.allclose(eig.first_sparse_pca(X), [[0.13573869], [0.99074467]]))
+        self.assertTrue(np.allclose(eig.first_sparse_pca(X), [0.13573869, 0.99074467]))
