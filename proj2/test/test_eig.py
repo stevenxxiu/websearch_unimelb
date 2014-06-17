6add5f31bc98b4f8f368ac417a16c4b07c4426e5
Steven Xu <xxu@student.unimelb.edu.au>
2014-06-18 00:27:25 +1000

    added first_sparse_pca()

--- proj2/test/test_eig.py
+++ proj2/test/test_eig.py
@@ -4,5 +4,9 @@ import numpy as np
 from proj2.lib.linalg import eig
 
 class TestEigen(unittest.TestCase):
-    def test_power_iter(self):
-        self.assertTrue(np.allclose(eig.power_iter(np.matrix([[1, 2], [3, 4]])), [[ 0.41597356], [ 0.90937671]]))
+    def test_first_eigenvector(self):
+        self.assertTrue(np.allclose(eig.first_eigenvector(np.matrix([[1, 2], [3, 4]])), [[0.41597356], [0.90937671]]))
+
+    def test_first_sparse_pca(self):
+        X = np.matrix([[ 1,  1], [2,  4], [3, 9], [ 4,  16], [ 5,  25], [ 6,  36]])
+        self.assertTrue(np.allclose(eig.first_sparse_pca(X), [[0.13573869], [0.99074467]]))
