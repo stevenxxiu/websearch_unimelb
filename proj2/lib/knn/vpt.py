423d31747a244c95234f03898191492a2ea16be1
Steven Xu <xxu@student.unimelb.edu.au>
2014-06-28 11:58:30 +1000

    changed _build_tree() so that history isn't taken into account, it doesn't serve any purpose

--- proj2/lib/knn/vpt.py
+++ proj2/lib/knn/vpt.py
@@ -3,14 +3,14 @@ import random
 import heapq
 import statistics
 import numpy as np
+from operator import itemgetter
 from collections import namedtuple
 
-TreeNode = namedtuple('TreeNode', ('p', 'left', 'right', 'bnds'))
-Item = namedtuple('Item', ('p', 'hist'))
+TreeNode = namedtuple('TreeNode', ('p', 'left', 'right', 'lower_bnd', 'upper_bnd'))
 
 class VPTree:
     '''
-    A vps tree.
+    A vp tree with subspace bounds.
     '''
     def __init__(self, distance):
         self.distance = distance
@@ -23,46 +23,37 @@ class VPTree:
         self.root = self._build_tree(np.array(range(X.shape[0])))
 
     def _build_tree(self, points):
-        items = list(Item(point, list()) for point in points)
-        return self._recurse_tree(items)
-
-    def _recurse_tree(self, items):
-        if not items:
+        if not points:
             return None
         node = TreeNode()
-        p_i = self._select_vp(items)
-        node.p = items[p_i]
-        items.pop(p_i)
-        # append all distances to the current vantage point to hist
-        for item in items:
-            item.hist.append(self.distance(node.p, item.p))
+        p_i = self._select_vp(points)
+        node.p = points[p_i]
+        points.pop(p_i)
+        # compute distances
+        dists = []
+        for point in points:
+            dists.append(self.distance(self.X[node.p], self.X[point]))
         # compute the median distance to the current vantage point
-        mu = statistics.median(item.hist[-1] for item in items)
-        left_items = []
-        right_items = []
-        for item in items:
-            if item.hist[-1] < mu:
-                left_items.append(item)
+        mu = statistics.median(dists)
+        left_points = []
+        right_points = []
+        left_dists = []
+        right_dists = []
+        for i, dist in enumerate(dists):
+            if dist < mu:
+                left_points.append(points[i])
+                left_dists.append(dist)
             else:
-                right_items.append(item)
-        node.left = self._recurse_tree(left_items)
-        node.right = self._recurse_tree(right_items)
-        # calculate bounds
-        hist_len = len(node.p.hist)
-        if node.left is not None and node.right is not None:
-            node.bnds = []
-            for lb, rb, pd in zip(node.left.bnds[:hist_len], node.right.bnds[:hist_len], node.p.hist):
-                node.bnds.append((min(lb[0], rb[0], pd), max(lb[1], rb[1], pd)))
-        elif node.left is not None:
-            node.bnds = []
-            for lb, pd in zip(node.left.bnds[:hist_len], node.p.hist):
-                node.bnds.append((min(lb[0], pd), max(lb[1], pd)))
-        elif node.right is not None:
-            node.bnds = []
-            for rb, pd in zip(node.right.bnds[:hist_len], node.p.hist):
-                node.bnds.append((min(rb[0], pd), max(rb[1], pd)))
-        else:
-            node.bnds = node.p.hist
+                right_points.append(points[i])
+                right_dists.append(dist)
+        # create children
+        node.left = self._build_tree(left_points)
+        node.right = self._build_tree(right_points)
+        # calculate bounds of each child node with regards to p
+        node.left.lower_bnd = min(left_dists)
+        node.left.upper_bnd = max(left_dists)
+        node.left.lower_bnd = min(left_dists)
+        node.left.upper_bnd = max(left_dists)
         return node
 
     def _select_vp(self, items):
