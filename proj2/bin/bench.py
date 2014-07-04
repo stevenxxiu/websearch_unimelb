c5c5dcf22edfc3c63ccbbb643aa8e7cf908a5af9
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-07-04 21:55:45 +0000

    added KNeighborsClassifier as a benchmark and to see if results match
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@2548 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- proj2/bin/bench.py
+++ proj2/bin/bench.py
@@ -1,9 +1,11 @@
 
 import time
 import pickle
+import numpy as np
 from workshops.lib.classif import get_train_test
 from workshops.lib.weights import l2_norm_sparse
 from workshops.lib.features import get_tf_idf
+from sklearn.neighbors import KNeighborsClassifier
 from proj2.lib.knn.pat import PrincipalAxisTree
 
 def main():
@@ -15,12 +17,20 @@ def main():
         train_indexes, test_indexes = get_train_test(len(docs_data.docs), 2000, 500)
         train_X = tf_idfs[train_indexes]
         test_X = tf_idfs[test_indexes]
+
         tree = PrincipalAxisTree(6)
         tree.fit(train_X)
         start = time.clock()
         res = tree.search(test_X[0], 40)
         print('Search took {} ms'.format(time.clock() - start))
         print(res)
+
+        tree = KNeighborsClassifier(40)
+        tree.fit(train_X, np.zeros(train_X.shape[0]))
+        start = time.clock()
+        res = tree.kneighbors(test_X[0], 40)
+        print('Search took {} ms'.format(time.clock() - start))
+        print(list(zip(res[0][0], res[1][0])))
 
 if __name__ == '__main__':
     main()
