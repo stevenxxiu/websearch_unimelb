f5a22c0cdc423342a79b89bbfbefaf4a58db6948
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-07-04 21:57:11 +0000

    fixed bugs in the naive approach; use the same output format to be more comparable
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@2566 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- proj2/bin/bench.py
+++ proj2/bin/bench.py
@@ -6,6 +6,7 @@ from workshops.lib.classif import get_train_test
 from workshops.lib.weights import l2_norm_sparse
 from workshops.lib.features import get_tf_idf
 from sklearn.neighbors import KNeighborsClassifier
+from proj2.lib.knn.brute import KNeighborsBrute
 from proj2.lib.knn.pat import PrincipalAxisTree
 
 def main():
@@ -18,23 +19,30 @@ def main():
         train_X = tf_idfs[train_indexes]
         test_X = tf_idfs[test_indexes]
 
-        tree = PrincipalAxisTree(6)
+        start = time.clock()
+        tree = PrincipalAxisTree(30)
         tree.fit(train_X)
+        print('Tree construction took {} s'.format(time.clock() - start))
         start = time.clock()
         res = tree.search(test_X[0], 40)
-        print('Search took {} ms'.format(time.clock() - start))
+        print('Search took {} s'.format(time.clock() - start))
         print(res)
 
         tree = KNeighborsClassifier(40, leaf_size=30)
         tree.fit(train_X, np.zeros(train_X.shape[0]))
         start = time.clock()
         res = tree.kneighbors(test_X[0], 40)
-        print('Search took {} ms'.format(time.clock() - start))
-        print(list(zip(res[0][0], res[1][0])))
+        res = list(zip(res[0][0], res[1][0]))
+        print('Search took {} s'.format(time.clock() - start))
+        print(res)
 
+        # to be fair, don't assume normalized
+        tree = KNeighborsBrute()
+        tree.fit(train_X)
         start = time.clock()
-        res = train_X*test_X[0].T
-        print('Search took {} ms'.format(time.clock() - start))
+        res = tree.kneighbors(test_X[0], 40)
+        print('Search took {} s'.format(time.clock() - start))
+        print(res)
 
 if __name__ == '__main__':
     main()
---
+++ proj2/lib/knn/brute.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+from workshops.lib.weights import l2_sparse
+
+class KNeighborsBrute:
+    def __init__(self):
+        self.X = None
+        self.X_l2_sq = None
+
+    def fit(self, X):
+        self.X = X
+        self.X_l2_sq = np.power(l2_sparse(X), 2)
+
+    def kneighbors(self, y, k=None):
+        dists = np.sqrt(self.X_l2_sq + np.power(l2_sparse(y), 2).T - 2*self.X*y.T)
+        if k is None:
+            k = self.X.shape[0]
+        indexes = np.argsort(dists)
+        return list(zip(dists[indexes[:k]], indexes[:k]))
