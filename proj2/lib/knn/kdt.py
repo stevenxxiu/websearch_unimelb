87a05e58be8024d1667a494760025f6b6b55f6a6
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-07-04 21:58:36 +0000

    implemented KDTree._build_tree()
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@2584 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

---
+++ proj2/lib/knn/kdt.py
@@ -0,0 +1,51 @@
+
+import numpy as np
+from collections import namedtuple
+
+TreeNode = namedtuple('Node', ('pivot', 'left', 'right'))
+
+class KDTree:
+    def __init__(self, distance):
+        self.distance = distance
+        self.X = None
+        self.root = None
+        self.n_traversed = 0
+
+    def fit(self, X):
+        self.X = X
+        self.root = self._build_tree(list(range(X.shape[0])), 0)
+
+    def _build_tree(self, points, depth):
+        if not points:
+            return None
+        # select axis based on depth so that axis cycles through all valid values
+        axis = depth % self.X.shape[1]
+        # sort points based on the current axis
+        points.sort(key=lambda i: self.X[i,axis])
+        # choose the median as the pivot
+        i = len(points)//2
+        # Create node and construct subtrees
+        return TreeNode(
+            pivot = points[i],
+            left = self._build_tree(points[:i], depth + 1),
+            right_child = self._build_tree(points[i + 1:], depth + 1)
+        )
+
+    def search(self, q, k):
+        # max heap
+        nearest = [(-np.inf, None)] * k
+        self._search(nearest, self.root, q)
+        nearest = list(nearest)
+        for i, (d, n) in enumerate(nearest):
+            nearest[i] = (-d, n)
+        return sorted(nearest)
+
+    def _search(self, nearest, node, q):
+        pass
+
+
+def main():
+    """Example usage"""
+    point_list = [(2,3), (5,4), (9,6), (4,7), (8,1), (7,2)]
+    tree = kdtree(point_list)
+    print(tree)
