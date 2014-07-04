aca28f1018bf350c41b13c1bc7989ad6ab66a077
xxu <xxu@1e191c3d-2cf9-4e68-b3ea-b23acec113d7>
2014-07-04 21:56:31 +0000

    added first_sparse_pca()
    git-svn-id: http://svn.codalism.com/svn/comp90042-2014s1/xxu@2558 1e191c3d-2cf9-4e68-b3ea-b23acec113d7

--- proj2/lib/linalg/eig.py
+++ proj2/lib/linalg/eig.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-def power_iter(X, max_iter=100, tol=1.0e-8):
+def first_eigenvector(X, max_iter=100, tol=1.0e-8):
     '''
     Find the eigenvector corresponding the the largest eigenvalue using power iteration.
     Doesn't converge if the largest eigenvalue has multiplicity > 1, but still can be used as an approximation tool.
@@ -10,6 +10,25 @@ def power_iter(X, max_iter=100, tol=1.0e-8):
     for i in range(max_iter):
         b_prev = b
         b = X * b
+        # normalize b
+        b *= 1/np.sqrt(np.sum(np.power(b, 2)))
+        # l1 norm
+        err = np.sum(b - b_prev)
+        if abs(err) < tol:
+            break
+    else:
+        raise ValueError('power iteration failed to converge in {} iterations'.format(max_iter))
+    return b
+
+def first_sparse_pca(X, max_iter=100, tol=1.0e-8):
+    '''
+    Sklearn's implementation does not normalize to mean 0 first.
+    '''
+    b = np.random.random_sample((X.shape[1], 1))
+    for i in range(max_iter):
+        b_prev = b
+        # calculate b = (X.T - X.mean(axis=0)).T * (X.T - X.mean(axis=0)) * b while preserving sparsity
+        b = X.T*(X*b) - np.sum(X, axis=0).T*(X.mean(axis=0)*b)
         # normalize b
         b *= 1/np.sqrt(np.sum(np.power(b, 2)))
         # l1 norm
